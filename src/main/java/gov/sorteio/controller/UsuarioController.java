efe9617337a01dae21326e4aad0f4cee464c0670
João Victor <joaovss1992@gmail.com>
2025-05-06 16:10:24 -0300

    removendo import desnecesário

--- src/main/java/gov/sorteio/controller/UsuarioController.java
+++ src/main/java/gov/sorteio/controller/UsuarioController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.UUID;
 
 @Controller
 @RequestMapping(path = "/v1/usuario")
